8608dc37aff2b58bfa8bf91481ff181188cfdbcf
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-09-01 17:08:12 -0400

    [Login] added files to commit  - controllers/api/user-routes.js  - views/login.handlebars

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User, Post, Comment } = require("../../models");
+const { User, Post, UsersFriends } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.get("/", (req, res) => {
@@ -81,7 +81,7 @@ router.post("/login", (req, res) => {
     },
   }).then((dbUserData) => {
     if (!dbUserData) {
-      res.status(400).json({ message: "No user with that username!" });
+      res.status(400).json({ message: "No user with that email address!" });
       return;
     }
 
