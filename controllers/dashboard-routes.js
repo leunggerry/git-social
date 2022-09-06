03a7b4ecf897e176a79927d759c13432ccac7808
gigi.upNorth <leung.gerry@gmail.com>
2022-09-06 14:13:28 -0400

    [dashboard] adding coments number to each post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -12,14 +12,14 @@ router.get("/", withAuth, (req, res) => {
     },
     attributes: ["id", "title", "text_body", "github_repo_url", "created_at"],
     include: [
-      // {
-      //   model: Comment,
-      //   attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-      //   include: {
-      //     model: User,
-      //     attributes: ["username"],
-      //   },
-      // },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
