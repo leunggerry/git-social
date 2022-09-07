67d887b0ba123bc59a1dc3ed3a98d9b3b3112ea0
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-09-07 16:46:42 -0400

    [UI UPDATE] Added icons     - Icons added to dashboard, post-info, and main handlebars     - Added favicon

--- seeds/friendship-seeds.js
+++ seeds/friendship-seeds.js
@@ -1,26 +1,53 @@
 const { Friendship } = require("../models");
 
 const friendshipData = [
-  {
-    requester_id: 1,
-    addressee_id: 2,
-    user_id: 1, // user_id = requester_id
-  },
+  // Friend requests to user 1
   {
     requester_id: 2,
     addressee_id: 1,
     user_id: 2, // user_id = requester_id
   },
   {
-    requester_id: 3,
-    addressee_id: 2,
-    user_id: 3, // user_id = requester_id
-  },
-  {
     requester_id: 4,
     addressee_id: 1,
     user_id: 4, // user_id = requester_id
   },
+  {
+    requester_id: 5,
+    addressee_id: 1,
+    user_id: 5, // user_id = requester_id
+  },
+  {
+    requester_id: 3,
+    addressee_id: 1,
+    user_id: 3, // user_id = requester_id
+  },
+  {
+    requester_id: 10,
+    addressee_id: 1,
+    user_id: 10, // user_id = requester_id
+  },
+  {
+    requester_id: 9,
+    addressee_id: 1,
+    user_id: 9, // user_id = requester_id
+  },
+  {
+    requester_id: 8,
+    addressee_id: 1,
+    user_id: 8, // user_id = requester_id
+  },
+  {
+    requester_id: 1,
+    addressee_id: 2,
+    user_id: 1, // user_id = requester_id
+  },
+  {
+    requester_id: 3,
+    addressee_id: 2,
+    user_id: 3, // user_id = requester_id
+  },
+
   {
     requester_id: 5,
     addressee_id: 2,
