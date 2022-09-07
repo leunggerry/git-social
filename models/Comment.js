645cdd1bb8fa95991ac07c5f76877d5b556f3765
gigi.upNorth <leung.gerry@gmail.com>
2022-09-07 14:02:55 -0400

    [jsdoc] Adding comments for documentation
    - adding html documentation and documentation config file

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,13 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
+/**
+ * @description Model representing the Comment table
+ * @param {int} id - auto-incrementing id for the comment table
+ * @param {string} comment_text - user input comment for the comment for a post
+ * @param {int} user_id - user id of the comment author
+ * @param {int} post_id - post id referencing the comment of the post is meant for
+ */
 class Comment extends Model {}
 
 Comment.init(
--- models/UsersFriends.js
+++ models/UsersFriends.js
@@ -1,6 +1,13 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
+/**
+ * @instance
+ * @description Model representing the User's friends
+ * @param {int} user_id - user id of the user
+ * @param {int} friend_id - id of the user that is a friend
+ */
+
 class UsersFriends extends Model {}
 
 UsersFriends.init(
